380a9d0c0acb0e61ad68f2a8944116fd337aeb0f
cov <brainstormmedia@ya.ru>
2023-01-29 01:58:06 +0500

    Form page done.

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, useTheme, Typography } from "@mui/material";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
